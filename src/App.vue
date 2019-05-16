0141ecc194f81ecbedeba418de6d081ec8938e88
Chris Rodgers <crodgers@alternativeairlines.com>
2019-05-16 16:41:26 +0100

    Work on offcanvas menu

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,15 @@
   <div id="app">
     <header class="header">
       <div class="wrapper">
-        <img class="header__logo" src="./assets/logo.svg">
+        <div class="header__inner">
+          <img class="header__logo" src="./assets/logo.svg">
+          <button class="header__button" v-on:click="open = !open">
+            <burger class="header__burger"/>
+          </button>
+        </div>
       </div>
     </header>
-    <div class="wrapper wrapper--offcanvas">
+    <div class="wrapper wrapper--offcanvas" v-bind:class="{'wrapper--active': open}">
       <aside-nav/>
       <main class="main">
         <div class="main__title">
@@ -23,11 +28,16 @@
 
 <script>
 import AsideNav from "@/modules/Aside";
+import Burger from "./assets/icons/Og Burger.svg?inline";
 
 export default {
   name: "App",
+  data() {
+    return { open: false };
+  },
   components: {
-    AsideNav
+    AsideNav,
+    Burger
   }
 };
 </script>
@@ -36,12 +46,32 @@ export default {
 .header {
   border-bottom: 1px solid #dedddc;
   margin-bottom: 40px;
+  &__inner {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
   &__logo {
     height: 40px;
     margin: 10px 20px;
     @include breakpoint("tablet") {
       height: 50px;
       margin: 15px 50px;
+    }
+  }
+  &__button {
+    line-height: 0;
+  }
+  &__burger {
+    width: 20px;
+    height: 20px;
+    margin: 20px;
+    line {
+      stroke: $blue;
+    }
+    @include breakpoint("mobile") {
+      display: none;
     }
   }
 }
@@ -57,12 +87,15 @@ export default {
       transform: translateX(-200px);
     }
   }
+  &--active {
+    transform: translateX(0);
+  }
 }
 .main {
   flex: 1;
   padding: 0 20px;
   min-width: 100vw;
-  @include breakpoint("mobile"){
+  @include breakpoint("mobile") {
     min-width: auto;
   }
   @include breakpoint("tablet") {
