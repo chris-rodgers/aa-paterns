fc0a07f17e3d4d17936e4b504bbd5fb9f51c0714
Chris Rodgers <crodgers@alternativeairlines.com>
2019-05-17 10:41:01 +0100

    Split takeaway directive into its own file

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App";
 import router from "./router";
 import Toasted from "vue-toasted";
 import { esacapeHtml, escapeHtml } from "./helpers";
+import takeaway from "./directives/takeway";
 
 Vue.config.productionTip = false;
 
@@ -15,37 +16,7 @@ Vue.use(Toasted, {
 });
 
 // Add takeaway to clipboard directive
-Vue.directive("takeaway", {
-	update: (el, binding, vnode) => {
-		el.setAttribute("data-takeaway", binding.value);
-	},
-	bind: (el, binding, vnode) => {
-		el.classList.add("takeaway", "takeaway--copy");
-		el.setAttribute("data-takeaway", binding.value);
-
-		el.addEventListener("click", () => {
-			const val = el.getAttribute("data-takeaway");
-
-			// Create new element
-			var textarea = document.createElement("textarea");
-			// Set value (string to be copied)
-			textarea.value = val;
-			// Set non-editable to avoid focus and move outside of view
-			textarea.setAttribute("readonly", "");
-			textarea.style = { position: "absolute", left: "-9999px" };
-			document.body.appendChild(textarea);
-			// Select text inside element
-			textarea.select();
-			// Copy text to clipboard
-			document.execCommand("copy");
-			// Remove temporary element
-			document.body.removeChild(textarea);
-			vnode.context.$toasted.show(
-				`'${escapeHtml(val)}' has been copied to your clipboard.`
-			);
-		});
-	}
-});
+Vue.directive("takeaway", takeaway);
 
 /* eslint-disable no-new */
 new Vue({
